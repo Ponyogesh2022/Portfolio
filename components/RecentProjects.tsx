2a4cfaa910bb34287c041ac69b863d23e72bd639
Ponyogesh P <digi-hive02@nipponpaint.co.in>
2025-09-30 14:24:36 +0530

    -feat: setup sentry for get user experience

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -6,8 +6,8 @@ import { FaLocationArrow } from "react-icons/fa6";
 const RecentProjects = () => {
   return (
     <div className="py-20" id="projects">
-      <h1 className="heading">
-        A small selection of
+      <h1 className="text-4xl">
+        A small selection of {""}
         <span className="text-purple">recent projects</span>
       </h1>
       <div className="flex flex-wrap  items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
--- components/ui/3d-pin.tsx
+++ components/ui/3d-pin.tsx
@@ -20,6 +20,7 @@ export const PinContainer = ({
     "translate(-50%,-50%) rotateX(0deg)"
   );
 
+  
   const onMouseEnter = () => {
     setTransform("translate(-50%,-50%) rotateX(40deg) scale(0.8)");
   };
@@ -66,7 +67,7 @@ export const PinPerspective = ({
   href?: string;
 }) => {
   return (
-    <motion.div className="pointer-events-none  w-full h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
+    <motion.div className="pointer-events-none w-full h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-10 transition duration-500">
       <div className=" w-full h-full -mt-7 flex-none  inset-0">
         <div className="absolute top-0 inset-x-0  flex justify-center">
           <a
